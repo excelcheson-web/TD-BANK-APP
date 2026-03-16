0a6504e4331b0b0310b1c0f81795e225c4e2ed98
Chekwube D. Ajasogu <excelcheson@gmail.com>
2026-03-16 04:52:23 -0700

    modified logo

--- src/components/TDLogo.jsx
+++ src/components/TDLogo.jsx
@@ -1,25 +1,6 @@
 /* TD Bank – Green shield logo as inline SVG */
 export default function TDLogo({ size = 48, className = '', style }) {
-  return (
-    <svg
-      width={size}
-      height={size}
-      viewBox="0 0 120 120"
-      fill="none"
-      className={className}
-      style={style}
-      aria-label="TD Bank logo"
-    >
-      {/* Shield shape */}
-      <path
-        d="M60 6C40 6 14 14 14 14v46c0 28 46 48 46 48s46-20 46-48V14S86 6 60 6z"
-        fill="#2d6a2e"
-      />
-      <path
-        d="M60 12C42 12 20 19 20 19v41c0 24 40 42 40 42s40-18 40-42V19S78 12 60 12z"
-        fill="#34a853"
-      />
-  // Use the official TD Bank SVG logo from Wikimedia
+  // Use the official TD Bank SVG logo from Wikimedia (HTTPS URL)
   return (
     <img
       src="https://upload.wikimedia.org/wikipedia/commons/a/a4/TD_Bank_logo.svg"
@@ -31,6 +12,5 @@ export default function TDLogo({ size = 48, className = '', style }) {
       aria-label="TD Bank logo"
       draggable="false"
     />
-    </svg>
   )
 }
